fc0e3374f11135ffa366c6bdfa505a0f3bb9ba1c
jlance94 <116045262+jlance94@users.noreply.github.com>
2022-12-23 18:17:43 -0500

    Create JLanceCodingchallengesubmission.js

---
+++ JLanceCodingchallengesubmission.js
@@ -0,0 +1,36 @@
+const findSum = function([2,4,6,8,10]) {
+   let total =0;
+    for( let i = 0; i < array.length; i++){
+        total += array[i];
+    }
+    return total;
+};
+
+const findFrequency = function(['a', 'b', 'c', 'a', 'b', 'c', 'a', 'a', 'd']) {
+  
+};
+
+const isPalindrome = function("tacocat") {
+    const arrayValues = string.split('');
+    const reverseArrayValues = arrayValues.reverse();
+    const reverseString = reverseArrayValues.join('');
+
+    if(string == reverseString) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+const largestPair = function(array) {
+ 
+};
+
+const removeParenth = function(str) {
+  
+};
+
+const scoreScrabble = function(str) {
+  
+};
